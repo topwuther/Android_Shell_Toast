e4ad7dfec7240f489bed0b20144163aa9b34b316
uinanfan32 <uinanfan32@gmail.com>
2021-06-23 14:46:13 +0800

    Add custom time

--- app/src/main/java/com.shell/toast/ToastReceiver.kt
+++ app/src/main/java/com.shell/toast/ToastReceiver.kt
@@ -3,11 +3,23 @@ package com.shell.toast
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.view.Gravity
 import android.widget.Toast
 
 class ToastReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         val data : String? = intent?.getStringExtra("msg")
-        Toast.makeText(context,data,Toast.LENGTH_SHORT).show()
+        val toast :Toast = Toast.makeText(context,data,Toast.LENGTH_SHORT)
+        if (intent != null) {
+            when (intent.getStringExtra("d")){
+                "short"->{
+                    toast.duration = Toast.LENGTH_SHORT
+                }
+                "long"->{
+                    toast.duration = Toast.LENGTH_LONG
+                }
+            }
+        }
+        toast.show()
     }
 }
